a619a829d8a7047e8ad8763724a83a23e7ea029f
Armin Kau <armin.mangafic95@gmail.com>
2026-08-15 04:01:10 +0200

    Use Railway-assigned PORT instead of hardcoded 3000

--- server/server.js
+++ server/server.js
@@ -282,4 +282,5 @@ app.get("/admin/ordrar", async (req, res) => {
   res.json(data);
 });
 
-app.listen(3000, () => console.log("Kvario-servern lyssnar på :3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Kvario-servern lyssnar på :${PORT}`));
